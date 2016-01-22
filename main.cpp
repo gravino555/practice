97c30b090ca51964bf3b4fdec4bf9168d7f3cc3b
Michael G <hockey_goals@hotmail.com>
2016-01-22 17:39:43 +0000

    changed abit of code

--- main.cpp
+++ main.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <sstream>
+#include <ostream>
 using namespace std;
 
-char cat;
+char catChoice = {0};
 double celsius = 0;
 double fahrenheit = 0;
+string input = "";
 
 int main(){
+    bool correctInput = false;
     cout << "Welcome to the practice app" <<endl;
     cout<<endl;
     cout << "Let's do a Celsius to Fahrenheit convertion app" << endl;
     cout<<endl;
     cout<<endl;
     cout << "Begin by entering if you want to convert from Fahrenheit to Celsius by pressing \"f\" or vice versa by typing \"c\": " << endl;
-    cin << cat ;
+    cin << catChoice ;
     
-    for (cat == f) {
+    if (catChoice == f) {
         cout << "Please enter the temperature in Fahrenheit that you would like to convert to Celsius: " << endl;
         cin << fahrenheit;
         celsius = (fahrenheit - 32) * 5/9;
         cout << fahrenheit << " degrees Fahrenheit is " << celsius << " degrees Celsius."<< endl;
     }
-    for (cat == c) {
+    else if (catChoice == c) {
         cout << "Please enter the temperature in Celsius that you would like to convert to Fahrenheit: " << endl;
         cin << celsius;
         fahrenheit = (celsius + 32) * 9/5;
         cout << celsius << " degrees Celsius is " << fahrenheit << " degrees Fahrenheit."<< endl;
     }
-    for (cat != f || cat != c) {
+    else {
         cout << "Option selected does not exist" << endl;
-        return main();
+        return 0;
     }
 }
